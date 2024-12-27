9064f297c38de9bf7f5ef3d8b886c869607d1aa0
David Bauer <89662560+davbauer@users.noreply.github.com>
2024-12-27 01:29:48 +0100

    Added never commited logs

---
+++ api/src/routes/logs/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from "hono";
+import { stream } from "hono/streaming";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { LogsModel } from "../../lib/models/LogsModel";
+
+export const logs = new Hono().get(
+  "/stream",
+  zValidator(
+    "query",
+    z.object({
+      maxLines: z.coerce.number().optional(),
+    })
+  ),
+  async (c) => {
+    const { maxLines } = c.req.valid("query");
+    c.header("Content-Encoding", "Identity");
+
+    return stream(c, async (writer) => {
+      const logsStream = await LogsModel.streamLogs(maxLines || undefined);
+      const reader = logsStream.getReader();
+
+      try {
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) break;
+          await writer.write(value);
+        }
+      } finally {
+        reader.releaseLock();
+      }
+    });
+  }
+);
